2d16c7b51026e5bf2a165b768f94904492669d60
huyhieu2612 <75487589+tailuong-perosi@users.noreply.github.com>
2022-05-09 09:08:24 +0700

    shopping_detail

--- be/routers/shoppingDairy.js
+++ be/routers/shoppingDairy.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const shopping_dairy = require(`../controllers/shopping_diary`);
 const { auth } = require(`../middleware/jwt`);
 
-router.route(`/update/:order_id`).patch(auth, shopping_dairy._update);
+
 router.route(`/:user_phone`).get(auth, shopping_dairy._get);
-router.route('/delete').delete(auth, shopping_dairy._delete);
+router.route(`/check/:name/:order_id`).get(auth, shopping_dairy._getOne);
+// router.route('/delete').delete(auth, shopping_dairy._delete);
 
 module.exports = router;
